328a99cc62018541b1fad2f3452870b38a0f9c22
Leon Windheuser <leon.windheuser@gmail.com>
2023-02-07 00:50:20 +0100

    add benchmark results

--- runSuccinctBenchmarks.sh
+++ runSuccinctBenchmarks.sh
@@ -2,11 +2,18 @@ touch benchmarks.csv
 echo $'name\trun\ttiming\tsizeInBytes\tMemoryAllocationInBytes' > benchmarks.csv
 
 build/benchmark/benchmark_runner SuccinctSequentialInsert 2>> benchmarks.csv
+build/benchmark/benchmark_runner SuccinctPaddedSequentialInsert 2>> benchmarks.csv
 build/benchmark/benchmark_runner NonSuccinctSequentialInsert 2>> benchmarks.csv
+
 build/benchmark/benchmark_runner SuccinctRandomInsert 2>> benchmarks.csv
+build/benchmark/benchmark_runner SuccinctPaddedRandomInsert 2>> benchmarks.csv
 build/benchmark/benchmark_runner NonSuccinctRandomInsert 2>> benchmarks.csv
+
 build/benchmark/benchmark_runner SuccinctNormalDistribution 2>> benchmarks.csv
+build/benchmark/benchmark_runner SuccinctPaddedNormalDistribution 2>> benchmarks.csv
 build/benchmark/benchmark_runner NonSuccinctNormalDistribution 2>> benchmarks.csv
+
 build/benchmark/benchmark_runner SuccinctZipfDistribution 2>> benchmarks.csv
+build/benchmark/benchmark_runner SuccinctPaddedZipfDistribution 2>> benchmarks.csv
 build/benchmark/benchmark_runner NonSuccinctZipfDistribution 2>> benchmarks.csv
 
